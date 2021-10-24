5ec85836c0bd6d5522f3bf99b1ab630379e3e643
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2021-10-23 22:47:40 -0300

    mobile-02

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
 
 import { Home } from './src/screens/Home';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    // caso as novas fontes não foram carregadas ele vai segurar a tela de reload.
+    return <AppLoading />
+  }
+
   return (
-    <Home />
+    <>
+      <StatusBar style="light" />
+      <Home />
+    </>
   );
 }
